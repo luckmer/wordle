3c91e006d992af61f24ce5b6ca8c9b9dec841870
luckmer <woszczo04@gmail.com>
2022-04-23 23:11:25 +0200

    refactor

---
+++ server/controllers/index.ts
@@ -0,0 +1,22 @@
+import {wordCheck,wordsOptions } from "../config/wordsConfig"
+import axios from 'axios'
+
+export const getWordsMatch = async (word:string)=>{
+    try{
+        const wordOptions=  wordCheck(word)
+        const responseJson = await axios.request(wordOptions)
+        return  await responseJson.data.result_msg
+    }catch (error){
+        console.error(`couldn't match words , ${error}`)
+    }
+}
+
+export const  getWordsResponse = async ()=>{
+    try{
+        const responseJson = await axios.request(wordsOptions)
+        return  await responseJson.data
+    }catch (error){
+        console.error(`couldn't get words , ${error}`)
+    }
+}
+
--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,4 @@
-import getWordsResponse from "./controllers/getWordsResponse";
-import getWordsMatch from "./controllers/getWordsMatch";
+import {getWordsResponse , getWordsMatch} from "./controllers";
 
 export const words = await getWordsResponse()
 export const  matchWord =  (word:string) =>  getWordsMatch(word)
