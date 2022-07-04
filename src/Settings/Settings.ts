5ab800a110d0f71a95b7472f963733d5f780d9c2
luckmer <woszczo04@gmail.com>
2022-07-04 20:43:02 +0200

    added close animation

--- src/Settings/Settings.ts
+++ src/Settings/Settings.ts
@@ -1,13 +1,37 @@
 const SettingsButton = document.querySelector(".settings_menu");
 const SettingsModal = document.querySelector(".settings_container_modal");
+const closeSettingsButton = document.querySelector(".close_settings");
 
 class SettingsClass {
-  handleControlSettings = () => SettingsModal?.classList.add("settings_open");
+  public static flag = false;
+
+  clearCloseAnimation = (SettingsModal: Element) => {
+    if (!SettingsModal.className.split(" ").includes("settings_close")) return;
+    console.log("remove");
+    SettingsModal.classList.remove("settings_open");
+    SettingsModal.classList.remove("settings_close");
+  };
+
+  handleOpenSettings = () => SettingsModal?.classList.add("settings_open");
+
+  handleCloseSettings = () => {
+    SettingsModal?.classList.add("settings_close");
+
+    SettingsModal?.addEventListener(
+      "animationend",
+      () => this.clearCloseAnimation(SettingsModal),
+      false
+    );
+  };
 
   initiateSettings = () => {
     (SettingsButton as Element).addEventListener(
       "click",
-      this.handleControlSettings
+      this.handleOpenSettings
+    );
+    (closeSettingsButton as Element).addEventListener(
+      "click",
+      this.handleCloseSettings
     );
   };
 }
