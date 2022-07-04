14620d508c445682bcf912a28a74b213b402cfb3
luckmer <woszczo04@gmail.com>
2022-07-04 21:55:31 +0200

    refactor

--- src/Settings/Settings.ts
+++ src/Settings/Settings.ts
@@ -1,9 +1,44 @@
 const SettingsButton = document.querySelector(".settings_menu");
 const SettingsModal = document.querySelector(".settings_container_modal");
 const closeSettingsButton = document.querySelector(".close_settings");
+const settingsSection = document.querySelector(".settings_sections");
 
 class SettingsClass {
   public static flag = false;
+  private settingsStructure = [
+    {
+      header: "Hard Mode",
+      description: "Any revealed hints must be used in subsequent guesses",
+    },
+    { header: "Hard Mode" },
+    { header: "High Contrast Mode", description: "For improved color vision" },
+  ];
+
+  initiateSettingsModal = () => {
+    this.settingsStructure.forEach((setting, index) => {
+      const div = document.createElement("div");
+      const htmStructure = `<div class="settings_content_container">
+        <div style="padding-right: 8px">
+          <div style="font-size: 18px"> ${setting.header}</div>
+          ${
+            setting.description &&
+            '<div style="font-size: 12px; color: #787c7e; margin-top: -2px">' +
+              setting.description +
+              "</div>"
+          }
+        </div>
+        <div style="padding-right: 7px">
+          <label
+            class="mdl-switch mdl-js-switch mdl-js-ripple-effect"
+            for=switch-${index}>
+            <input type="checkbox" id="switch-${index}" class="mdl-switch__input" />
+          </label>
+        </div>
+      </div>`;
+      div.innerHTML = htmStructure;
+      settingsSection?.appendChild(div);
+    });
+  };
 
   clearCloseAnimation = (SettingsModal: Element) => {
     if (!SettingsModal.className.split(" ").includes("settings_close")) return;
@@ -25,6 +60,8 @@ class SettingsClass {
   };
 
   initiateSettings = () => {
+    this.initiateSettingsModal();
+
     (SettingsButton as Element).addEventListener(
       "click",
       this.handleOpenSettings
